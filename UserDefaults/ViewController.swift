20b3649ee5859b301b54e105a03d46428c65059d
Mahmudul Hasan Zunnun <33092475+znun@users.noreply.github.com>
2023-07-29 11:25:26 +0600

    todo list add

---
+++ UserDefaults/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  UserDefaults
+//
+//  Created by Mahmudul Hasan on 17/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var statusLbl: UILabel!
+    @IBOutlet weak var myTable: UITableView!
+    
+    var todoArray = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let data = UserDefaults.standard.object(forKey: "todoList") as? [String] {
+            todoArray = data
+            statusLbl.text = "\(todoArray.count) task pending on todo list"
+        } else {
+            statusLbl.text = "No task Pending"
+        }
+       
+    }
+
+   
+    @IBAction func saveBtn(_ sender: Any) {
+        
+        if textField.text != "" {
+            todoArray.append(textField.text!)
+            UserDefaults.standard.set(todoArray, forKey: "todoList")
+            myTable.reloadData()
+            statusLbl.text = "New status added on todo list"
+            textField.text = ""
+        } else {
+            statusLbl.text = "Kindly enter status first"
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTable.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = todoArray[indexPath.row]
+        return cell
+    }
+    
+}
+
